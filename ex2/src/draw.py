8f5233e9f6e94befe35451bd6d6bc1a0f1ba8005
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2019-12-14 21:21:41 +0800

    add auto select color

--- ex2/src/draw.py
+++ ex2/src/draw.py
@@ -1,18 +1,28 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import random
+
+char = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
 
 # s 点集合
 # tag 在图上的表示
+def get_random_color():
+    color = "#"
+    for i in range(6):
+        color = color + str(random.sample(char,1)[0])
+    return color
+
 def draw( s : list, name : str):
     plt.clf()
     plt.xlim((-10,10))
     plt.ylim((-10,10))
+    map = {}
     for point in s:
-        if point[2] == 0:
-            plt.scatter(point[0], point[1], marker = "+", color="r")
-        if point[2] == 1:
-            plt.scatter(point[0], point[1], marker = "+", color="g")
-        if point[2] == 2:
-            plt.scatter(point[0], point[1], marker = "+", color="b")    
+        if point[2] in map.keys():
+            color = map[point[2]]
+        else:
+            color = get_random_color()
+            map[point[2]] = color
+        plt.scatter(point[0], point[1], marker = "+", color=color)
     plt.savefig(name)
